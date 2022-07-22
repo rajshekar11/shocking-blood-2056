fb4c7f34bedd28bd64973fbeaa7b02b506e3d4ea
chandan055 <cssingh@gmail.com>
2022-07-23 00:23:36 +0530

    day-3

---
+++ day3/login.js
@@ -0,0 +1,24 @@
+function login() {
+    let username = document.getElementById("user_name").value;
+    let password = document.getElementById("password").value;
+    let data = JSON.parse(localStorage.getItem("forlogin"))
+    // console.log(data[0].password)
+    for (let i = 0; i < data.length; i++) {
+        // console.log(data[i].user_name)
+
+
+
+        if (data[i].user_name == username) {
+            console.log(data[i].user_name)
+            if (data[i].password == password) {
+                console.log(data[i].password)
+                alert("loging successfull")
+            }
+            else{
+                alert("wrong password") 
+            }
+        }
+    }
+
+
+}
--- script/index.js
+++
@@ -1,7 +0,0 @@
-import { navbar } from "./navbar.js";
-
-  document.getElementById("navb").innerHTML = navbar();
-
-  import footer from "./footer.js";
-
-document.getElementById("footer_main").innerHTML=footer()
