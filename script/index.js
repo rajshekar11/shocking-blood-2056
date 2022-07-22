85a6cc635b2708abb73b31eb6b75edae1ff36649
rajshekar11 <rajshekar767@gmail.com>
2022-07-23 01:08:03 +0530

    index.js updated

--- script/index.js
+++ script/index.js
@@ -1,10 +1,7 @@
 import { navbar } from "./navbar.js";
 
-<<<<<<< HEAD
-=======
   document.getElementById("navb").innerHTML = navbar();
 
   import footer from "./footer.js";
 
 document.getElementById("footer_main").innerHTML=footer()
->>>>>>> 6319ca06ee8cb32dc2b9465ed2da0b8a7dcf0a27
