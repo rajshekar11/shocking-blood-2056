7a6b17e35de386c2011dec393aba717e86e99ae5
chandan055 <cssingh@gmail.com>
2022-07-23 11:01:58 +0530

    files being added back

---
+++ script/footer.js
@@ -0,0 +1,239 @@
+
+
+let footer=()=>{
+    return `  <div class="footer_top">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path
+        fill="#0f1b29"
+        fill-opacity="1"
+        d="M0,0L1440,96L1440,320L0,320Z"
+      ></path>
+    </svg>
+    <div class="footer_top_innercontent">
+      <div class="footer_top_contents">
+        <div>
+          <a href="https://about.codecov.io/">
+            <img
+              src="https://about.codecov.io/wp-content/themes/codecov/assets/brand/icons/codecov/codecov-circle.svg"
+              alt=""
+            />
+          </a>
+        </div>
+        <div class="footer_innercontent_secondimg">
+          <a href="https://www.linkedin.com/company/codecov/">
+            <img
+              style="background-color: white; border-radius: 5px"
+              src="https://cdn3.iconfinder.com/data/icons/social-media-black-white-2/512/BW_Linkedin_glyph_svg-128.png"
+              alt=""
+            />
+          </a>
+          <a href="https://twitter.com/codecov">
+            <img
+              style="background-color: white; border-radius: 60%"
+              src="https://cdn1.iconfinder.com/data/icons/social-media-circle-7/512/Circled_Twitter_svg-128.png"
+              alt=""
+            />
+          </a>
+          <a href="https://github.com/codecov">
+            <img
+              style="background-color: white; border-radius: 50%"
+              src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+              alt=""
+            />
+          </a>
+          <a
+            href="https://www.youtube.com/channel/UCj-fJuvXv-m05HkSNn0OS9g/featured"
+          >
+            <img
+              style="background-color: white; border-radius: 100%"
+              src="https://cdn3.iconfinder.com/data/icons/social-media-black-white-2/512/BW_Youtube_glyph_svg-128.png"
+              alt=""
+            />
+          </a>
+          <a href="https://angel.co/company/codecov">
+            <img
+              style="color: white"
+              src="https://cdn2.iconfinder.com/data/icons/hand-gestures-63/512/hand-fingers-sign-gesture-12-128.png"
+              alt=""
+            />
+          </a>
+          <a href="https://community.codecov.io/">
+            <img
+              style="background-color: white; border-radius: 100%"
+              src="https://cdn.iconscout.com/icon/free/png-64/discourse-3626955-3029363.png"
+              alt=""
+            />
+          </a>
+          <a href="https://www.instagram.com/codecov/">
+            <img
+              style="background-color: white; border-radius: 100%"
+              src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Instagram_svg-128.png"
+              alt=""
+            />
+          </a>
+        </div>
+      </div>
+      <!-- top inner content ends here -->
+      <div class="footer_top_body_content">
+        <table class="table_content">
+          <thead>
+            <tr>
+              <th>
+                <a href="https://about.codecov.io/product/features/"
+                  >PRODUCT</a
+                >
+              </th>
+              <th>
+                <a href="https://about.codecov.io/solutions/"
+                  >SOLUTIONS</a
+                >
+              </th>
+              <th>
+                <a href="https://about.codecov.io/customers/"
+                  >CUSTOMERS</a
+                >
+              </th>
+              <th>
+                <a href="https://about.codecov.io/resources/"
+                  >RESOURCES</a
+                >
+              </th>
+              <th><a href="https://about.codecov.io/">COMPANY</a></th>
+              <th>
+                <a href="https://about.codecov.io/contact/">CONTACT US</a>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <a href="https://about.codecov.io/product/features/"
+                  >Features</a
+                >
+              </td>
+              <td>
+                <a href="https://about.codecov.io/for/open-source/"
+                  >Open Source</a
+                >
+              </td>
+              <td>
+                <a href="https://about.codecov.io/customers/"
+                  >Customers</a
+                >
+              </td>
+              <td>
+                <a
+                  href="https://about.codecov.io/resources/?_type=webinar%2Con-demand-webinar"
+                  >Webinars</a
+                >
+              </td>
+              <td><a href="https://about.codecov.io/team/">Team</a></td>
+              <td>
+                <a href="https://about.codecov.io/contact/">Contact</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <a href="https://about.codecov.io/product/integrations/"
+                  >Integrations</a
+                >
+              </td>
+
+              <td>
+                <a href="https://about.codecov.io/for/enterprises/"
+                  >Enterprise</a
+                >
+              </td>
+              <td>
+                <a
+                  href="https://about.codecov.io/resources/?_type=case-study"
+                  >Case Studies</a
+                >
+              </td>
+              <td><a href="https://about.codecov.io/blog/">Blog</a></td>
+              <td>
+                <a href="https://about.codecov.io/company/press/"
+                  >Press</a
+                >
+              </td>
+              <td><a href="https://about.codecov.io/demo/">Demo</a></td>
+            </tr>
+            <tr>
+              <td>
+                <a href="https://about.codecov.io/product/documentation/"
+                  >Documentation</a
+                >
+              </td>
+              <td>
+                <a href="https://about.codecov.io/for/startups/"
+                  >Startups</a
+                >
+              </td>
+              <td>
+                <a href="https://about.codecov.io/resources/community/"
+                  >Community</a
+                >
+              </td>
+              <td>
+                <a href="https://about.codecov.io/product/documentation/"
+                  >Documentation</a
+                >
+              </td>
+              <td>
+                <a href="https://about.codecov.io/careers/"
+                  >Careers We're Hiring</a
+                >
+              </td>
+              <td>
+                <a href="https://about.codecov.io/support/">Support</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <a href="https://about.codecov.io/product/api/">API</a>
+              </td>
+              <td>
+                <a href="https://about.codecov.io/for/education/"
+                  >Education</a
+                >
+              </td>
+              <td></td>
+              <td></td>
+              <td>
+                <a href="https://about.codecov.io/write-for-us/"
+                  >Write for us</a
+                >
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <a href="https://about.codecov.io/product/status/"
+                  >Status</a
+                >
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+  <div class="footer_bottom">
+    <div class="footer_bottom_main">
+      <div id="google_translate_element"></div>
+      <div class="footer_bottom_mid_div">
+        <p>
+          <a href="https://about.codecov.io/terms-and-conditions/"
+            >Terms & Conditions</a
+          >
+        </p>
+        <p><a href="https://about.codecov.io/privacy/">Privacy</a></p>
+        <p><a href="https://about.codecov.io/security/">Security</a></p>
+        <p><a href="https://about.codecov.io/eula/">EULA</a></p>
+        <p><a href="https://about.codecov.io/gdpr/">GDPR</a></p>
+      </div>
+      <div><p>© Codecov 2022</p></div>
+    </div>
+  </div>`
+}
+
+export default footer;
